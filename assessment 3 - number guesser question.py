f8bc607400fdbb9bb17d21e9195700782438776d
greymerlion <gtheis@gmail.com>
2019-02-01 20:37:44 +0800

    Update assessment 3 - number guesser question.py

--- assessment 3 - number guesser question.py
+++ assessment 3 - number guesser question.py
@@ -1,5 +1,8 @@
-# This is a game where we generate a random number between 1 to 100
-# user will need to guess the number
-# for each guess, we will tell the user if the number is lower or higher
-# after the user has guessed correctly, we will congratulate the user
+# This is a program that plays a number guessing game.
+# The program should generate a random number between 1 to 100
+# You must ask the user to guess the number
+# The program should loop until the user has correctly guessed the number
+# For each guess, we will tell the user if the number is lower or higher
+# If the user has guessed correctly, we will congratulate the user
 # and print out the number of tries the user took.
+# If the user has guessed incorrectly, the program should ask the user to guess again
