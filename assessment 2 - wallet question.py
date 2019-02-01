05bf6baebab10b7c7a85fa00f91bc25d98daedee
greymerlion <gtheis@gmail.com>
2019-02-01 13:56:01 +0800

    Add files via upload

---
+++ assessment 2 - wallet question.py
@@ -0,0 +1,8 @@
+# create a class of wallet
+# in wallet, there should be three features
+# give(x) give x number of coins
+# a wallet cannot give more than the coins it has
+# receive(x) take in x number of coins
+# a wallet cannot store more than 20 coins
+# print_information() prints the values of how much a user starts with, 
+# how much they have given and received and how much they currently have
---
+++ assessment 3 - number guesser question.py
@@ -0,0 +1,5 @@
+# This is a game where we generate a random number between 1 to 100
+# user will need to guess the number
+# for each guess, we will tell the user if the number is lower or higher
+# after the user has guessed correctly, we will congratulate the user
+# and print out the number of tries the user took.
