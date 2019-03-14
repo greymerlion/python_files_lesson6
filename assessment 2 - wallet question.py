99f9ea644d92ad8f3feef5817012fc14f9af2827
greymerlion <gtheis@gmail.com>
2019-03-14 13:24:50 +0800

    Update assessment 2 - wallet question.py

--- assessment 2 - wallet question.py
+++ assessment 2 - wallet question.py
@@ -1,22 +1,24 @@
+# Assessment 2
 # create a class of Wallet
 # in wallet, there should be three features
-# the wallet should have a self variable for coin count
+# the wallet should have a self variable for coin balance
 # you should be able to initialize the wallet's coin count with a number, else it should default to 0
-# the wallet should have a funtion/method give(x) - give x number of coins out of the wallet
+# the wallet should have a function/method remove_money_from_wallet(x) - remove x number of coins out of the wallet
 # NOTE: a wallet cannot give out more than the coins it has, it should fail to give out and print an error
-# the wallet should have a funtion/method  receive(x) -- receive x number of coins into the wallet
-# NOTE: a wallet cannot store more than 100 coins, it should not accept the give and print an error
-# print_amount() prints the current number of coins the wallet has 
+# the wallet should have a function/method  put_money_into_wallet(x) -- put x number of coins into the wallet
+# NOTE: a wallet cannot store more than 100 coins, it should *not accept* the full amount that would push it over 
+# and print an error
+# print_balance() prints the current number of coins the wallet has 
 # you must run the following tests on your wallet
 
 my_wallet = Wallet(25)
-my_wallet.receive(10)
-my_wallet.give(5)
-my_wallet.receive(20)
-print(my_wallet.print_amount()) # answer should be 50
+my_wallet.put_money_into_wallet(10)
+my_wallet.remove_money_from_wallet(5)
+my_wallet.put_money_into_wallet(20)
+print(my_wallet.print_balance()) # answer should be 50
 
 your_wallet = Wallet()
-your_wallet.receive(25)
-your_wallet.receive(100) # this should fail with a message
-your_wallet.give(30)  # this should fail with a message
-print(your_wallet.print_amount()) # answer should be 25
+your_wallet.put_money_into_wallet(25)
+your_wallet.put_money_into_wallet(100) # this should fail with a message
+your_wallet.remove_money_from_wallet(30)  # this should fail with a message
+print(your_wallet.print_balance()) # answer should be 25
